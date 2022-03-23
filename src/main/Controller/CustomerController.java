322052f1f01b45e6665209896ea953f6d503d91f
mdtanvirhasan <56194099+mdtanvirhasan@users.noreply.github.com>
2022-03-23 12:53:59 +0600

    Add files via upload

---
+++ src/main/Controller/CustomerController.java
@@ -0,0 +1,26 @@
+package Customer.src.main.Controller;
+
+import Customer.src.main.Service.CustomerService;
+@AllArgsConstructor
+@NoArgsConstructor
+@RestController
+public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    private String name;
+    private Long cardnumber;
+    private Long mobileNumber;
+    private 
+
+    public searchPizza(){
+
+    }
+
+    public paymentByCard(){
+
+    }
+    public paymentBymKash(){
+
+    }
+}
